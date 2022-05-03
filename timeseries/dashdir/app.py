f901f1b427815f3794cadf6ba13f979433244104
IddoSadeh <44779994+IddoSadeh@users.noreply.github.com>
2022-05-03 16:13:52 -0700

    change small comment + test commit

--- timeseries/dashdir/app.py
+++ timeseries/dashdir/app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Run this app with `python app3.py` and
+# Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 # documentation at https://dash.plotly.com/
 
