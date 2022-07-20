6a4686b809d0aa196e5e839457c0a7d0df294f19
IddoSadeh <44779994+IddoSadeh@users.noreply.github.com>
2022-07-19 18:24:26 -0700

    added coloring option

--- sketch-annotation/dashdir/app.py
+++ sketch-annotation/dashdir/app.py
@@ -2,7 +2,8 @@ import re
 import plotly.graph_objects as go
 # import plotly.express as px
 import dash
-from dash import Dash, dcc, html, Input, Output, State
+from dash import Dash, dcc, html, Input, Output, State, ctx
+import dash_daq as daq
 from skimage import data
 import json
 
@@ -88,7 +89,7 @@ app.layout = html.Div(
     [
         html.H3("Drag and draw annotations - use the modebar to pick a different drawing tool"),
         dcc.Graph(id="fig-image", figure=fig, config=config),
-
+        dcc.Store(id='relay', storage_type='session'),
         html.Pre(id="annotations-data-pre"),
         dcc.Input(id="text-input", type='text'),
         html.Button('add text to image', id='submit-val', n_clicks=0),
@@ -96,6 +97,10 @@ app.layout = html.Div(
         dcc.ConfirmDialog(
             id='confirm-reset',
             message='Warning! All progress wil be lost! Are you sure you want to continue?',
+        ),
+        html.Pre('Color Picker, a represent opacity on a scale of 0 (opaque) to 1 (fully filled)'),
+        daq.ColorPicker(
+            id="annotation-color-picker", label="Color Picker", value=dict(rgb=dict(r=0, g=0, b=0, a=0))
         ),
 
     ]
@@ -110,103 +115,144 @@ def display_confirm(submit, reset):
     if reset and not submit:
         return True
     if reset and submit:
-        if reset>submit:
+        if reset > submit:
             return True
     return False
 
 
 @app.callback(
     Output("fig-image", "figure"),
-    Output('submit-val','n_clicks'),
+    Output('submit-val', 'n_clicks'),
     Output('confirm-reset', 'submit_n_clicks'),
     Output('clean-reset', 'n_clicks'),
-    Input('fig-image', 'relayoutData'),
+    State('fig-image', 'relayoutData'),
     State('text-input', 'value'),
     Input('submit-val', 'n_clicks'),
     Input('confirm-reset', 'submit_n_clicks'),
     Input('clean-reset', 'n_clicks'),
+    Input("annotation-color-picker", "value"),
 )
-def save_data(relayout_data, inputText, submit_clicks, confirm,reset):
+def save_data(relayout_data, inputText, submit_clicks, confirm, reset, color_value):
+    print("new")
     # adding new text
     if submit_clicks:
-         if not len(fig.layout.annotations) == submit_clicks:
-            fig.add_annotation(
-                text=inputText,
-                showarrow=False,
-                font=dict(
-                    family="Courier New, monospace",
-                    size=28,
-                    color="white",
+        if not len(fig.layout.annotations) == submit_clicks:
+            return add_text(inputText, color_value), submit_clicks, confirm, reset
 
-                ),
-                x=img_width / 4,
-                y=img_height / 2.5,
-            )
-            return fig, submit_clicks, confirm, reset
-
-    # relayout_data gives back user changes data
-    print("relay")
-    print(relayout_data)
+    # relayout_data gives back user changes data, if it exists, update changes to figure
+    if "dragmode" in str(relayout_data):
+        fig.update_layout(relayout_data)
     if relayout_data:
+        update_annotations(relayout_data, color_value)
 
-        # adding or removing shapes
-        if "'shapes':" in str(relayout_data):
-            counter = 0
-            fig.layout.shapes = ()
-            # all shapes on screen will be returned in relay data upon new shape creation.
-            for i in relayout_data['shapes']:
-                fig.add_shape(i)
+    print(relayout_data)
+    if ctx.triggered_id == "annotation-color-picker":
+        print('herrr')
+        print(relayout_data)
+        print(color_value)
+        update_annotations(relayout_data, color_value)
+        return fig, submit_clicks, confirm, reset
 
-        # changing shapes
-        elif "shapes[" in str(relayout_data):
-            # using regex to find which shape was changed
-            shape_num_index = re.search(r"\d", str(relayout_data))
-            i = int(str(relayout_data)[shape_num_index.start()])
-
-            # changing dictionary keys so we can update the shape change easily
-            dictnames = list(relayout_data.keys())
-            new_dict = {}
-            counter = 0
-            for name in dictnames:
-                dictnames[counter] = name[10:]
-                counter = counter + 1
-            for key, n_key in zip(relayout_data.keys(), dictnames):
-                new_dict[n_key] = relayout_data[key]
-            fig.update_shapes(new_dict, i)
-
-        # if text is changed, "annotations" wil be part of the relayout data
-        elif "annotations" in str(relayout_data):
-            fig.update_annotations(captureevents=True)
-            # using regex to find which annotation was changed
-            anno_num_index = re.search(r"\d", str(relayout_data))
-            i = int(str(relayout_data)[anno_num_index.start()])
-
-            # if text content is changed "text" will be in relay data
-            if "text" in str(relayout_data):
-                fig.update_annotations(Annotation(fig.layout.annotations[i]['x'], fig.layout.annotations[i]['y'],
-                                                  relayout_data[f'annotations[{i}].text']).__dict__, i)
-
-            # if text is just moved relay data wont have "text" in data
-            else:
-                fig.update_annotations(
-                    Annotation(relayout_data[f'annotations[{i}].x'], relayout_data[f'annotations[{i}].y'],
-                               fig.layout.annotations[i]['text']).__dict__, i)
-
-    # resetting image
+    # resetting image if confirm was clicked
     if confirm:
+        return clean_figure(confirm)
+
+    return fig, submit_clicks, confirm, reset
+
+
+# precondition: inputText is not none and the figure has more annotations on it than those saved in figure data
+# postcondition: add inputText to figure data
+def add_text(inputText, color_value):
+    r = color_value['rgb']['r']
+    g = color_value['rgb']['g']
+    b = color_value['rgb']['b']
+    a = color_value['rgb']['a']
+    fig.add_annotation(
+        text=inputText,
+        showarrow=False,
+        font=dict(
+            family="Courier New, monospace",
+            size=28,
+            color=f'rgba({r},{g},{b},{1})',
+
+        ),
+        x=img_width / 4,
+        y=img_height / 2.5,
+    )
+    return fig
+
+
+# preconditions: relayout_data is not none
+# function updates annotations for figure
+def update_annotations(relayout_data, color_value):
+    r = color_value['rgb']['r']
+    g = color_value['rgb']['g']
+    b = color_value['rgb']['b']
+    a = color_value['rgb']['a']
+
+    if "'shapes':" in str(relayout_data):
+        relayout_data['shapes'][-1]["fillcolor"] = f'rgba({r},{g},{b},{a})'
         fig.layout.shapes = ()
-        fig.layout.annotations = ()
-        return fig, 0, 0, 0
+        # all shapes on screen will be returned in relay data upon new shape creation.
+        for i in relayout_data['shapes']:
+            fig.add_shape(i)
+    # changing shapes
+    elif "shapes[" in str(relayout_data):
+        # using regex to find which shape was changed
+        shape_num_index = re.search(r"\d", str(relayout_data))
+        i = int(str(relayout_data)[shape_num_index.start()])
 
-    return dash.no_update, submit_clicks, confirm, reset
+        # changing dictionary keys so we can update the shape change easily
+        dictnames = list(relayout_data.keys())
+        new_dict = {}
+        counter = 0
+        for name in dictnames:
+            dictnames[counter] = name[10:]
+            counter = counter + 1
+        for key, n_key in zip(relayout_data.keys(), dictnames):
+            new_dict[n_key] = relayout_data[key]
+        new_dict["fillcolor"] = f'rgba({r},{g},{b},{a})'
+        fig.update_shapes(new_dict, i)
 
+    # if text is changed, "annotations" wil be part of the relayout data
+    elif "annotations" in str(relayout_data):
+        fig.update_annotations(captureevents=True)
+        # using regex to find which annotation was changed
+        anno_num_index = re.search(r"\d", str(relayout_data))
+        i = int(str(relayout_data)[anno_num_index.start()])
+
+        # if text content is changed "text" will be in relay data
+        if "text" in str(relayout_data):
+            fig.update_annotations(Annotation(fig.layout.annotations[i]['x'], fig.layout.annotations[i]['y'],
+                                              relayout_data[f'annotations[{i}].text'], f'rgba({r},{g},{b},1)').__dict__,
+                                   i)
+
+        # if text is just moved relay data wont have "text" in data
+        else:
+            fig.update_annotations(
+                Annotation(relayout_data[f'annotations[{i}].x'], relayout_data[f'annotations[{i}].y'],
+                           fig.layout.annotations[i]['text'], f'rgba({r},{g},{b},1)').__dict__, i)
+
+
+# precondition: confirm is not none
+# postcondition: all shape and text annotation data in figure is erase, all button click counters are reset
+def clean_figure(confirm):
+    fig.layout.shapes = ()
+    fig.layout.annotations = ()
+    return fig, 0, 0, 0
 
 
 class Annotation:
-    def __init__(self, x, y, text):
+    def __init__(self, x, y, text, color):
         self.x = x
         self.y = y
         self.text = text
+        self.font = dict(
+            family="Courier New, monospace",
+            size=28,
+            color=color,
+
+        )
 
 
 if __name__ == "__main__":
